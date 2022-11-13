f1d5d882a3c76a184ecee237497491279e437dee
Emiliano <Emiliano-R-D-L-C@outlook.com>
2022-11-12 19:02:33 -0500

    database should work

--- api/api.py
+++ api/api.py
@@ -1,14 +1,28 @@
 import time
-from flask import Flask
-from flask_pymongo import PyMongo
+#from flask import Flask
+from flask import Flask,jsonify, render_template,request,redirect
+#from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://sc_delaEmi:u2JsEd0nzYssgaMd@cluster0.8qczawe.mongodb.net/test', 5000)
-db = client['Software_Constrution']
-project = db.Sample
+
+
 
 app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://sc_delaEmi:u2JsEd0nzYssgaMd@cluster0.8qczawe.mongodb.net/test', 5000)
+db=client['test']
+project = db.Sample
 
 @app.route('/hello')
 def hello():
     return 'Hello world'
+
+
+@app.route('/mongotest', methods=["POST", "GET"])
+def mongotest():
+    id = "prk"
+    data = "dta"
+    packet = {"id": id, "data": data}
+    post_id = project.insert_one(packet).inserted_id
+    return "Added to db"
+
